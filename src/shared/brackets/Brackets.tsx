ef2036e20cd189fd2a43d1429d49b784f14c7029
nawneet <nawneet@rapidinnovation.dev>
2023-02-12 00:08:15 +0530

    code refactor

--- src/shared/brackets/Brackets.tsx
+++ src/shared/brackets/Brackets.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react'
 
 import { StageTile } from "./style"
-import { MatchCard } from "./components/MatchCard"
-import { Container, Row, MatchPairCard, BorderBoxWrapper, Column, BorderBox, BorderBoxTop, BorderBoxWrapperLeft } from "./style"
-import { gapMapping, sortArrayOfObjects } from '../utils';
+import { Container, Row, } from "./style"
+import { sortArrayOfObjects } from '../utils';
 import { data } from './data';
 import { SEMI_FINAL_1, SEMI_FINAL_2 } from '../text';
+import { RoundColumn } from './components/RoundColumn';
 
 const PARTION_LIMIT = 8
 export const Brackets = () => {
@@ -138,71 +138,4 @@ export const Brackets = () => {
             }
         </Row>
     </Container>
-}
-
-
-interface I_RoundColumnProps {
-    round: any
-    roundIndex: number
-    lastRoundNum: number
-    setHighlightedUserId: any
-    highlightedUserId: string
-    highlightedRoundNum: any
-    setHighlightedRoundNum: any
-    isCol2?: boolean
-    isPartioned?: boolean
-
-}
-const RoundColumn = (props: I_RoundColumnProps) => {
-    const { isCol2, isPartioned, round, roundIndex, lastRoundNum, setHighlightedUserId, highlightedUserId, setHighlightedRoundNum, highlightedRoundNum } = props
-
-    const highlightBox = (matches: any, isbox: boolean = false) => {
-        if (!matches?.participants || !highlightedUserId) return false
-        const resp = matches?.participants?.some((el: any) => el.id === highlightedUserId && (el.isWinner || !isbox))
-        return resp
-    }
-    const gapMapping2: any = gapMapping.slice(0, lastRoundNum - 1).reverse()
-
-    return (
-        <Column gap='36px'>
-            {
-                Object.entries(round).map(
-                    ([pairId, matches]: any, idx) => {
-                        return (
-                            <MatchPairCard key={idx} gap={isCol2 ? gapMapping2[roundIndex] : gapMapping[roundIndex]}>
-                                {
-                                    matches.map((match: any, matchIdx: number) => {
-                                        return <MatchCard
-                                            setHighlightedUserId={setHighlightedUserId}
-                                            setHighlightedRoundNum={setHighlightedRoundNum}
-                                            match={match}
-                                            matchIdx={matchIdx}
-                                            key={match.id || `${roundIndex}-${pairId}-${matchIdx}`}
-                                            roundIndex={roundIndex}
-                                            pairId={pairId}
-                                            highlightedRoundNum={highlightedRoundNum}
-                                            highlightedUserId={highlightedUserId}
-                                            isLineActive={highlightBox(matches[matchIdx])}
-                                            isCol2={isCol2}
-                                            isPartioned={isPartioned}
-                                        />
-                                    })
-                                }
-                                {isCol2
-                                    ? (roundIndex > 0 && roundIndex + 1 < lastRoundNum) && <BorderBoxWrapperLeft>
-                                        <BorderBoxTop className={highlightBox(matches[0], true) ? 'hover top topLeft' : 'topLeft'} isActive={highlightBox(matches[0], true)} />
-                                        <BorderBox className={highlightBox(matches[1], true) ? 'hover bottom bottomLeft' : 'bottomLeft'} isActive={highlightBox(matches[1] || {}, true)} />
-                                    </BorderBoxWrapperLeft>
-                                    : roundIndex + (isPartioned ? 2 : 1) < lastRoundNum && <BorderBoxWrapper>
-                                        <BorderBoxTop className={highlightBox(matches[0], true) ? 'hover top topRight' : 'topRight'} isActive={highlightBox(matches[0], true)} />
-                                        <BorderBox className={highlightBox(matches[1], true) ? 'hover bottom bottomRight' : 'bottomRight'} isActive={highlightBox(matches[1] || {}, true)} />
-                                    </BorderBoxWrapper>
-                                }
-                            </MatchPairCard>
-                        )
-                    }
-                )
-            }
-        </Column>
-    )
 }
---
+++ src/shared/brackets/components/RoundColumn.tsx
@@ -0,0 +1,69 @@
+import { gapMapping } from "../../utils"
+import { BorderBox, BorderBoxTop, BorderBoxWrapper, BorderBoxWrapperLeft, Column, MatchPairCard } from "../style"
+import { MatchCard } from "./MatchCard"
+
+interface I_RoundColumnProps {
+    round: any
+    roundIndex: number
+    lastRoundNum: number
+    setHighlightedUserId: any
+    highlightedUserId: string
+    highlightedRoundNum: any
+    setHighlightedRoundNum: any
+    isCol2?: boolean
+    isPartioned?: boolean
+
+}
+export const RoundColumn = (props: I_RoundColumnProps) => {
+    const { isCol2, isPartioned, round, roundIndex, lastRoundNum, setHighlightedUserId, highlightedUserId, setHighlightedRoundNum, highlightedRoundNum } = props
+
+    const highlightBox = (matches: any, isbox: boolean = false) => {
+        if (!matches?.participants || !highlightedUserId) return false
+        const resp = matches?.participants?.some((el: any) => el.id === highlightedUserId && (el.isWinner || !isbox))
+        return resp
+    }
+    const gapMapping2: any = gapMapping.slice(0, lastRoundNum - 1).reverse()
+
+    return (
+        <Column gap='36px'>
+            {
+                Object.entries(round).map(
+                    ([pairId, matches]: any, idx) => {
+                        return (
+                            <MatchPairCard key={idx} gap={isCol2 ? gapMapping2[roundIndex] : gapMapping[roundIndex]}>
+                                {
+                                    matches.map((match: any, matchIdx: number) => {
+                                        return <MatchCard
+                                            setHighlightedUserId={setHighlightedUserId}
+                                            setHighlightedRoundNum={setHighlightedRoundNum}
+                                            match={match}
+                                            matchIdx={matchIdx}
+                                            key={match.id || `${roundIndex}-${pairId}-${matchIdx}`}
+                                            roundIndex={roundIndex}
+                                            pairId={pairId}
+                                            highlightedRoundNum={highlightedRoundNum}
+                                            highlightedUserId={highlightedUserId}
+                                            isLineActive={highlightBox(matches[matchIdx])}
+                                            isCol2={isCol2}
+                                            isPartioned={isPartioned}
+                                        />
+                                    })
+                                }
+                                {isCol2
+                                    ? (roundIndex > 0 && roundIndex + 1 < lastRoundNum) && <BorderBoxWrapperLeft>
+                                        <BorderBoxTop className={highlightBox(matches[0], true) ? 'hover top topLeft' : 'topLeft'} isActive={highlightBox(matches[0], true)} />
+                                        <BorderBox className={highlightBox(matches[1], true) ? 'hover bottom bottomLeft' : 'bottomLeft'} isActive={highlightBox(matches[1] || {}, true)} />
+                                    </BorderBoxWrapperLeft>
+                                    : roundIndex + (isPartioned ? 2 : 1) < lastRoundNum && <BorderBoxWrapper>
+                                        <BorderBoxTop className={highlightBox(matches[0], true) ? 'hover top topRight' : 'topRight'} isActive={highlightBox(matches[0], true)} />
+                                        <BorderBox className={highlightBox(matches[1], true) ? 'hover bottom bottomRight' : 'bottomRight'} isActive={highlightBox(matches[1] || {}, true)} />
+                                    </BorderBoxWrapper>
+                                }
+                            </MatchPairCard>
+                        )
+                    }
+                )
+            }
+        </Column>
+    )
+}
